316ef450230a23e430eb6fe8745d212467e6f65c
Arunkumar <151753357+Arunkumar-Dhanraj@users.noreply.github.com>
2024-06-04 10:51:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import requests
 @click.argument('filename')
 def send_script(filename):
     """Sends a script to the server for execution."""
-    url = 'https://479f3ea6c9631.notebooksc.jarvislabs.net/run-script/'  
+    url = 'https://3ac95a2cb4ce1.notebooksb.jarvislabs.net/run-script/'  
     with open(filename, 'rb') as f:
         files = {'file': f}
         response = requests.post(url, files=files)
         print("Server response:", response.text)
 
-# if __name__ == '__main__':
-#     send_script()
+if __name__ == '__main__':
+    send_script()
     
-# https://479f3ea6c9631.notebooksc.jarvislabs.net/
+# https://3ac95a2cb4ce1.notebooksb.jarvislabs.net/
