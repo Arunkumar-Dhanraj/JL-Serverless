f0bf6b8a5194fec8d93ccfceb2b5002413b87078
Arunkumar <151753357+Arunkumar-Dhanraj@users.noreply.github.com>
2024-06-04 11:09:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='jl-serverless',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Arunkumar',
+    author_email='arun19ict@gmail.com',
     description='A CLI tool to run scripts on a remote server.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/jl-serverless',
+    url='https://github.com/Arunkumar-Dhanraj/JL-Serverless.git',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[
