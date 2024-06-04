d1b3500a7f16c45786490511e3ee4b47ccf380f6
Arunkumar <151753357+Arunkumar-Dhanraj@users.noreply.github.com>
2024-06-04 11:08:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,28 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name='jl-serverless',
     version='0.1.0',
-    author='Arunkumar',
-    author_email='arun19ict@gmail.com',
-    description='A simple CLI tool to run scripts remotely in JarvisLabs.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A CLI tool to run scripts on a remote server.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Arunkumar-Dhanraj/JL-Serverless.git',
+    url='https://github.com/yourusername/jl-serverless',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires='>=3.6',
     install_requires=[
         'click',
         'requests',
-        'fastapi',
-        'uvicorn'
     ],
     entry_points={
         'console_scripts': [
-            'jl-cli=jl_serverless.cli:send_script'
+            'jl-serverless=jl_serverless.cli:send_script'
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Organization :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
